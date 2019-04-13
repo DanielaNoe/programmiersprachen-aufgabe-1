80a92513c7397db775e4b16b281a9d38c42ce56b
DanielaNoe <daniela-noe@gmx.de>
2019-04-13 13:03:50 +0200

    Repository für Aufgabe 1 angelegt

---
+++ source/aufgabe1_3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+//main_________________________________________________________________________
+int
+main (int argc , char * argv []) {
+	std::cout << "Aufgabe 1.3" << std::endl;
+   	return 0;
+}//main
+
